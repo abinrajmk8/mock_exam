ed2f407ae728a6a6d51c87554eac55a82bcc6db8
Abin Raj <abinrajmk8@gmail.com>
2025-04-20 18:57:52 +0530

    h

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for proper routing
-  build: {
-    outDir: 'dist',
-  }
+  base: '',
 });
