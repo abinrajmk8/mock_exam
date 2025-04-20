aee943544e2a9b496321766041b85ce2599cb56e
Abin Raj MK <119695983+abinrajmk8@users.noreply.github.com>
2025-04-20 19:58:13 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
-//  const BACKEND_URL = 'https://mock-exam-backend.onrender.com';
+ const BACKEND_URL = 'https://mock-exam-backend.onrender.com';
 
 
 
 
- const BACKEND_URL = 'http://localhost:5000';
+ // const BACKEND_URL = 'http://localhost:5000';
 
 export default BACKEND_URL;
